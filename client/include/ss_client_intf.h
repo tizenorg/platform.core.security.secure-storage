371bd2f771ac4a3e4dc38cf68c99ef4af30564dc
Jinkun Jang <jinkun.jang@samsung.com>
2013-03-13 01:51:33 +0900

    Tizen 2.1 base

---
+++ client/include/ss_client_intf.h
@@ -0,0 +1,68 @@
+/*
+ * secure storage
+ *
+ * Copyright (c) 2000 - 2012 Samsung Electronics Co., Ltd All Rights Reserved 
+ *
+ * Contact: Kidong Kim <kd0228.kim@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __SS_MANAGER__
+#include "ss_manager.h"
+#endif
+
+/*
+ * Declare new function
+ *
+ *  @name: SsClientDataStore
+ *  @parameter
+ *     - filepath: [in]
+ *     - flag: [in]
+ *  @return type: int
+ *     - 1: success
+ *     - <1: error
+ */
+int SsClientDataStoreFromFile(const char* filepath, ssm_flag flag, const char* group_id);
+int SsClientDataStoreFromBuffer(char* writebuffer, size_t bufLen, const char* filename, ssm_flag flag, const char* group_id);
+
+/*
+ * Declare new function
+ *
+ *  @name: SsClientDataRead
+ *  @parameter
+ *     - filepath: [in]
+ *     - pRetBuf: [out]
+ *     - bufLen: [in]
+ *     - readLen: [out]
+ *  @return type: int
+ *     - 1: success
+ *     - <1: error
+ */
+int SsClientDataRead(const char* filepath, char* pRetBuf, size_t bufLen, size_t *readLen, ssm_flag flag, const char* group_id);
+
+/*
+ * Declare new function
+ *
+ *  @name: SsClientGetInfo
+ *  @parameter
+ *     - filepath: [in]
+ *     - sfi: [out]
+ *  @return type: int
+ *     - 1: success
+ *     - <1: error
+ */
+int SsClientGetInfo(const char* filepath, ssm_file_info_t* sfi, ssm_flag flag, const char* group_id);
+
+int SsClientDeleteFile(const char* pFilePath, ssm_flag flag, const char* group_id);
---
+++ client/src/ss_manager.c
@@ -0,0 +1,214 @@
+/*
+ * secure storage
+ *
+ * Copyright (c) 2000 - 2012 Samsung Electronics Co., Ltd All Rights Reserved 
+ *
+ * Contact: Kidong Kim <kd0228.kim@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "secure_storage.h"
+#include "ss_client_intf.h"
+
+#ifndef SS_API
+#define SS_API __attribute__((visibility("default")))
+#endif
+
+/*****************************************************************************
+ * Internal Functions
+ *****************************************************************************/
+SS_API
+int ssm_getinfo(const char* pFilePath, ssm_file_info_t *sfi, ssm_flag flag, const char* group_id)
+{
+	int ret = 0;
+
+	if(!pFilePath || !sfi)
+	{
+		SLOGE("Parameter error in ssm_getinfo()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+	
+	ret = SsClientGetInfo(pFilePath, sfi, flag, group_id);
+
+	if(ret == 1)
+	{
+		SLOGI("Getinfo Success.\n");
+		ret = 0;	// return true
+	}
+	else
+		SLOGE("Getinfo Fail.\n");
+
+Error:
+	return -(ret);
+}
+
+/*****************************************************************************
+ * Manager APIs
+ *****************************************************************************/
+SS_API
+int ssm_write_file(const char* pFilePath, ssm_flag flag, const char* group_id)
+{
+	int ret = 0;
+
+	if(!pFilePath)
+	{
+		SLOGE("Parameter error in ssm_write_file()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+	
+	if(flag <= SSM_FLAG_NONE || flag >= SSM_FLAG_MAX)
+	{
+		SLOGE("Parameter error in ssm_write_file()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+	
+	ret = SsClientDataStoreFromFile(pFilePath, flag, group_id);
+	if(ret == 1)
+	{
+		if(unlink(pFilePath) != 0)	// if fail
+		{
+			SLOGE("unlink fail. [%s]\n", pFilePath);
+			return -1;	// return false
+		}
+		SLOGI("Write file Success.\n");
+		return 0;	// return true
+	}
+	else
+		SLOGE("Write file Fail.\n");
+	
+Error:
+	return -(ret);
+}
+
+SS_API
+int ssm_write_buffer(char* pWriteBuffer, size_t bufLen, const char* pFileName, ssm_flag flag, const char* group_id)
+{
+	int ret = 0;
+
+	if(!pWriteBuffer || !pFileName || (pFileName[0] == '/'))
+	{
+		SLOGE("Parameter error in ssm_write_buffer()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+	if(bufLen <= 0 || bufLen > 4096)
+	{
+		SLOGE("Parameter error in ssm_write_buffer()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+	if(flag <= SSM_FLAG_NONE || flag >= SSM_FLAG_MAX)
+	{
+		SLOGE("Parameter error in ssm_write_buffer()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+
+	ret = SsClientDataStoreFromBuffer(pWriteBuffer, bufLen, pFileName, flag, group_id);
+	if(ret == 1)
+	{
+		SLOGI("Write buffer Success.\n");
+		return 0;	// return true
+	}
+	else
+		SLOGE("Write buffer Fail.\n");
+
+Error:	
+	return -(ret);
+}
+
+SS_API
+int ssm_read(const char* pFilePath, char* pRetBuf, size_t bufLen, size_t *readLen, ssm_flag flag, const char* group_id)
+{
+	int ret = 0;
+	ssm_file_info_t sfi;
+
+	if(!pFilePath || !pRetBuf)
+	{
+		SLOGE("Parameter error in ssm_read()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+	if(!readLen)
+	{
+		SLOGE("Parameter error in ssm_read()...\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+
+	// get info 
+	ret = ssm_getinfo(pFilePath, &sfi, flag, group_id);
+	if(ret != 0)	// ret != true?
+	{
+		SLOGE("getinfo error in ssm_read()..\n");
+		goto Error;
+	}
+	// in case of flag mismatch...
+	// check flag...
+	// To do :
+	if((bufLen > sfi.originSize) || (sfi.reserved[0] != (flag & 0x000000ff)))
+	{
+		SLOGE("Flag mismatch or buffer length error in ssm_read()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+
+	ret = SsClientDataRead(pFilePath, pRetBuf, sfi.originSize, readLen, flag, group_id);
+
+	if(ret == 1)
+	{
+		SLOGI("Read Success.\n");
+		return 0;	// return true
+	}
+	else
+		SLOGE("Read Fail.\n");
+
+Error:
+	return -(ret);
+}
+
+SS_API
+int ssm_delete_file(const char *pFilePath, ssm_flag flag, const char* group_id)
+{
+	int ret = 0;
+
+	if(!pFilePath)
+	{
+		SLOGE("Parameter error in ssm_delete_file()..\n");
+		ret = SS_PARAM_ERROR;
+		goto Error;
+	}
+
+	ret = SsClientDeleteFile(pFilePath, flag, group_id);
+
+	if(ret == 1)	// success
+	{
+		SLOGI("Delete file Success.\n");
+		return 0;
+	}
+	else	// fail
+		SLOGE("Delete file Fail.\n");
+
+Error:
+	return -(ret);
+}
